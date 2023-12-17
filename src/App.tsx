fdc7bdafb88561307e3d620b4ab434d99fb87a1a
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2023-12-17 17:38:01 +0900

    react_query 기본옵션 변경
    feat : react_query 기본옵션 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import RouterProvider from "Provider/RouterProvider";
 import ToastProvider from "Provider/ToastProvider";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnMount: "always",
+    },
+  },
+});
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
